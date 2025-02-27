065b2ae3da59543d997c9972630fd5a7db14c868
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-02-27 11:00:26 +0100

    remove config, use argparse instead

--- wginteractive/utility/wghandler.py
+++ wginteractive/utility/wghandler.py
@@ -1,32 +1,31 @@
 import os
 
 from ..classes.wginterface import WireGuardInterface
-from ..classes.config import Config
 
 
 class WireGuardHandler:
 
     interfaces: dict[str, WireGuardInterface] = {}
-    _config: Config = None
+    _wireguard_config_dir: str
 
     WIREGUARD_CONFIG_EXTENSION = "conf"
 
-    def __init__(self, config: Config) -> None:
-        self._config = config
+    def __init__(self, wireguard_config_dir: str) -> None:
+        self._wireguard_config_dir = wireguard_config_dir
 
     def get_interfaces(self) -> dict[str, WireGuardInterface]:
         if not self.interfaces:
             _ = []
             extension = f".{self.WIREGUARD_CONFIG_EXTENSION}"
 
-            for file in os.listdir(self._config.wireguard_conf_dir):
+            for file in os.listdir(self._wireguard_config_dir):
                 if file.endswith(extension):
                     _.append(file[: -len(extension)])
 
             _.sort()
             for config in _:
                 self.interfaces[config] = WireGuardInterface(
-                    config, self._config.wireguard_conf_dir
+                    config, self._wireguard_config_dir
                 )
 
         return self.interfaces
--- wginteractive/wginteractive.py
+++ wginteractive/wginteractive.py
@@ -2,15 +2,53 @@ import sys
 import os
 import logging
 from termcolor import colored
+import argparse
+from sys import version_info
 
 from ._version import __version__
 from .utility.clihandler import CliHandler
 
-
 def main():
+    # set up logging config via argparse
+    # custom behaviour for python versions < 3.11 as the level names mapping func was only added to the logging lib in 3.11
+    if version_info[1] >= 11:
+        loglevel_mapping = logging.getLevelNamesMapping().keys()
+    else:
+        loglevel_mapping = logging._nameToLevel.keys()
+
+    available_levels = [level.lower() for level in loglevel_mapping]
+    available_levels.remove(logging.getLevelName(logging.NOTSET).lower())
+    available_levels.remove(logging.getLevelName(logging.WARNING).lower())
+
+    parser = argparse.ArgumentParser(
+        description="An interactive command line tool for modifying and initializing WireGuard server configuration files and adding/deleting peers.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-l",
+        "--logging",
+        help="Set the log level",
+        dest="loglevel",
+        type=str,
+        choices=available_levels,
+        default=logging.getLevelName(logging.INFO).lower(),
+    )
+
+    parser.add_argument(
+        "-d",
+        "--directory",
+        help="WireGuard config directory",
+        type=str,
+        required=False,
+        default="/etc/wireguard",
+    )
+    
+    args = parser.parse_args()
+    
     logging.basicConfig(
         format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
-        level=logging.DEBUG,
+        level=args.loglevel.upper(),
     )
     logger = logging.getLogger(__name__)
 
@@ -29,7 +67,7 @@ An interactive command line tool for modifying and initializing WireGuard server
 
         print(banner)
 
-        handler = CliHandler()
+        handler = CliHandler(args.directory)
         handler.handle()
 
     except NotImplementedError as e:
