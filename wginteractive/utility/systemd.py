2773bff87d208238fffb8314b4e3d737703a7266
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-02-27 01:56:35 +0100

    add start and stop methods

--- wginteractive/utility/systemd.py
+++ wginteractive/utility/systemd.py
@@ -10,6 +10,9 @@ class Systemd:
     CMD_ENABLE = "enable"
     CMD_DISABLE = "disable"
     
+    CMD_START = "start"
+    CMD_STOP = "stop"
+    
     WG_QUICK_SERVICE = "wg-quick"
     
     def __init__(self) -> None:
@@ -18,7 +21,20 @@ class Systemd:
     @staticmethod
     def check_if_wg_interface_is_enabled(interface) -> bool | None:
         return Systemd.check_if_unit_is_enabled(f"{Systemd.WG_QUICK_SERVICE}@{interface}")
-
+    @staticmethod
+    def start_unit(unit: str):
+        if not unit.endswith(".service"):
+            unit = f"{unit}.service"
+        
+        Systemd.invoke_systemd_command_on_unit(Systemd.CMD_START, unit)
+    
+    @staticmethod
+    def stop_unit(unit: str):
+        if not unit.endswith(".service"):
+            unit = f"{unit}.service"
+        
+        Systemd.invoke_systemd_command_on_unit(Systemd.CMD_STOP, unit)
+    
     @staticmethod
     def flip_enabled_status(unit: str) -> None:
         if not unit.endswith(".service"):
